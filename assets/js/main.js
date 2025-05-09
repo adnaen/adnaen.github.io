e07869eb734780f297aa59fed0bfb34eab2aa142
Adnan <adnanmuhammedadnancp@gmail.com>
2025-05-10 03:34:36 +0530

    update the entire structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,42 +3,6 @@
   "use strict";
 
   /**
-   * Header toggle
-   */
-  const headerToggleBtn = document.querySelector('.header-toggle');
-
-  function headerToggle() {
-    document.querySelector('#header').classList.toggle('header-show');
-    headerToggleBtn.classList.toggle('bi-list');
-    headerToggleBtn.classList.toggle('bi-x');
-  }
-  headerToggleBtn.addEventListener('click', headerToggle);
-
-  /**
-   * Hide mobile nav on same-page/hash links
-   */
-  document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.header-show')) {
-        headerToggle();
-      }
-    });
-
-  });
-
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
-  });
-
-  /**
    * Preloader
    */
   const preloader = document.querySelector('#preloader');
@@ -220,3 +184,15 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".project-image").forEach(div => {
+      const imgSrc = div.dataset.imageSrc;
+      if (imgSrc) {
+        div.style.background = `
+          linear-gradient(to bottom, rgba(16,16,16,0.7), rgba(16,16,16,1)),
+          url('${imgSrc}')
+        `;
+      }
+    });
+  });
